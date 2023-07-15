b71d181997a8f54299d7f42519954c2a75ea30bd
Niranjan <niranjanverma823@gmail.com>
2023-07-15 23:30:32 +0530

    responsive added

--- frontend/src/components/InfoHeader.jsx
+++ frontend/src/components/InfoHeader.jsx
@@ -2,13 +2,17 @@ import { Box, Typography , styled} from '@mui/material'
 import React from 'react'
 
 
-const Container = styled(Box)`
-background:#f44336;
-color: #fff;
-display : flex;
-align-items: center;
-height:48px;
-`
+const Container = styled(Box)(({theme})=>({
+background:'#f44336',
+color: '#fff',
+display : 'flex',
+alignItems: 'center',
+height:48,
+marginBottom:30,
+[theme.breakpoints.down('md')]:{
+  display:'none'
+}
+}))
 
 const Image = styled('img')({
     height:34,
